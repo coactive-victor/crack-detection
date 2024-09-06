fd4f5e6887e76a0c6201df7944d385287fbda62d
coactive-victor <victor@coactive.ai>
2024-09-05 21:50:14 -0300

    feat: upload file to s3 bucket

--- app/routers/detection/router.py
+++ app/routers/detection/router.py
@@ -1,19 +1,42 @@
-from fastapi import APIRouter, Depends, status
-from mypy_boto3_s3 import S3Client  # Import type hint for S3 client
+from uuid import uuid4
+
+from fastapi import APIRouter, Depends, File, UploadFile, status
+from mypy_boto3_s3 import S3Client
 from sqlalchemy.orm import Session
 
 from app.db.database import get_session
 from app.db.s3 import get_s3_client
+from app.settings import Settings
 
 router = APIRouter(prefix="/detection", tags=["detection"])
+
+settings = Settings()
 
 
 @router.post(
     path="/",
     status_code=status.HTTP_202_ACCEPTED,
 )
-def create_user(
+async def detect_image(
     session: Session = Depends(get_session),
     s3_client: S3Client = Depends(get_s3_client),
+    file: UploadFile = File(...),
 ):
-    return {"message": "Hello, World!"}
+    image_id = uuid4()
+
+    # Get the file's content and metadata
+    file_content = await file.read()
+    file_extension = file.filename.split(".")[-1]
+    unique_filename = f"{image_id}.{file_extension}"
+    content_type = file.content_type
+
+    inbound_folder = settings.INBOUND_FOLDER
+    s3_path = f"{inbound_folder}/{unique_filename}"
+    s3_client.put_object(
+        Bucket=settings.BUCKET_NAME,
+        Key=s3_path,
+        Body=file_content,
+        ContentType=content_type,
+    )
+
+    return {"message": "File uploaded successfully", "filename": unique_filename}
--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    # Database credentials
     DATABASE_URL: str
 
+    # AWS Credentials
     AWS_ACCESS_KEY: str
     AWS_SECRET_KEY: str
+
+    # Bucket info
+    BUCKET_NAME: str
+    INBOUND_FOLDER: str
+    OUTBOUND_FOLDER: str
